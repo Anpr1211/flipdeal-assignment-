9cb6717c28f3e024ff343b01093888b48969a583
Anpr1211 <ankita.prakash1998@gmail.com>
2024-12-04 19:10:34 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const { resolve } = require('path');
+
+const app = express();
+const port = 3010;
+
+let taxRate = 5;
+let discountPercentage = 10;
+let loyaltyRate = 2;
+
+app.get('/cart-total', (req, res) => {
+  let newItemPrice = parseFloat(req.query.newItemPrice);
+  let cartTotal = parseFloat(req.query.cartTotal);
+  let TotalCartPrice = newItemPrice + cartTotal;
+  res.send(TotalCartPrice.toString());
+});
+
+app.get('/membership-discount', (req, res) => {
+  let cartTotal = parseFloat(req.query.cartTotal);
+  let isMember = req.query.isMember;
+  if (isMember === 'true') {
+    let discount = cartTotal * (discountPercentage / 100);
+    let discountedTotal = cartTotal - discount;
+    res.send(discountedTotal.toString());
+  }
+});
+
+app.get('/calculate-tax', (req, res) => {
+  let cartTotal = parseFloat(req.query.cartTotal);
+  let tax = cartTotal * (taxRate / 100);
+  res.send(tax.toString());
+});
+
+app.get('/estimate-delivery', (req, res) => {
+  let shippingMethod = req.query.shippingMethod;
+  let distance = parseFloat(req.query.distance);
+  let day = 0;
+  if (shippingMethod === 'Standard') {
+    day = distance / 50;
+  } else {
+    day = distance / 100;
+  }
+  res.send(day.toString());
+});
+
+app.get('/shipping-cost', (req, res) => {
+  let weight = parseFloat(req.query.weight);
+  let distance = parseFloat(req.query.distance);
+  let shippingCost = weight * distance * 0.1;
+  res.send(shippingCost.toString());
+});
+
+app.get('/loyalty-points', (req, res) => {
+  let purchaseAmount = parseFloat(req.query.purchaseAmount);
+  let loyaltyPoints = purchaseAmount * loyaltyRate;
+  res.send(loyaltyPoints.toString());
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
